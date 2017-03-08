3f929f51022879add1142a1dc6913f0d124bbe0a
Sergey Glazyrin <sergey.glazyrin.dev@gmail.com>
2017-03-08 02:56:07 +0100

    added more operations to KEY model

--- src/redisext/_key.py
+++ src/redisext/_key.py
@@ -5,7 +5,7 @@ from future.utils import with_metaclass
 class Key(with_metaclass(RedisKeyHandler, object)):
 
     allowed_commands = ('exists', 'expire', 'expireat', 'pexpire',
-                        'ttl', 'pttl', 'setnx')
+                        'ttl', 'pttl', 'setnx', 'incr', 'incrby')
 
     @classmethod
     def delete(cls, key):
