13f4116be4fbdaaf8669b1d2b4c59ac16d37feec
Sergey Glazyrin <sergey.glazyrin.dev@gmail.com>
2017-03-20 10:29:30 +0100

    minor fixes

--- src/redisext/_key.py
+++ src/redisext/_key.py
@@ -4,8 +4,8 @@ from future.utils import with_metaclass
 
 class Key(with_metaclass(RedisKeyHandler, object)):
 
-    allowed_commands = ('exists', 'expire', 'expireat', 'pexpire',
-                        'ttl', 'pttl', 'setnx', 'incr', 'incrby')
+    allowed_commands = ('exists', 'expire', 'expireat', 'pexpire', 'get',
+                        'ttl', 'pttl', 'setnx', 'incr', 'incrby', 'decr', 'decrby')
 
     @classmethod
     def delete(cls, key):
